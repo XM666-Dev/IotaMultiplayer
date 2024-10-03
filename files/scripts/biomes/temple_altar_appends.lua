e9537a0d9c980baef67c36c18621ee8c7d5dcb16
ImmortalDamned <99076011+XM666-Dev@users.noreply.github.com>
2024-10-03 22:43:31 +0800

    Fix

--- files/scripts/biomes/temple_altar_appends.lua
+++ files/scripts/biomes/temple_altar_appends.lua
@@ -7,16 +7,16 @@ dofile_once("mods/iota_multiplayer/files/scripts/lib/sule.lua")(function()
 		for i, entity in ipairs(EntityGetInRadius(x, y, 16)) do
 			local filename = EntityGetFilename(entity)
 			if filename == "data/entities/items/pickup/heart_fullhp_temple.xml" or filename == "data/entities/items/pickup/spell_refresh.xml" then
-				EntityAddComponent2(entity, "LuaComponent", {
-					script_item_picked_up = "mods/iota_multiplayer/files/scripts/items/share_pickup.lua",
-					execute_every_n_frame = -1,
-				})
 				if filename == "data/entities/items/pickup/heart_fullhp_temple.xml" then
 					EntityAddComponent2(entity, "LuaComponent", {
 						script_item_picked_up = "mods/iota_multiplayer/files/scripts/items/respawn_pickup.lua",
 						execute_every_n_frame = -1,
 					})
 				end
+				EntityAddComponent2(entity, "LuaComponent", {
+					script_item_picked_up = "mods/iota_multiplayer/files/scripts/items/share_pickup.lua",
+					execute_every_n_frame = -1,
+				})
 			end
 		end
 	end
--- files/scripts/items/respawn_pickup.lua
+++ files/scripts/items/respawn_pickup.lua
@@ -1,10 +1,8 @@
 dofile_once("mods/iota_multiplayer/files/scripts/lib/utilities.lua")
 
 function item_pickup(entity_item, entity_pickupper, item_name)
-    local item = EntityGetFirstComponent(entity_item, "ItemComponent")
-    if item == nil then return end
-    local x, y = ComponentGetValue2(item, "spawn_pos")
-    local respawn = EntityGetInRadiusWithTag(x, y, 16, "coop_respawn")[1]
+    local x, y = EntityGetTransform(entity_pickupper)
+    local respawn = EntityGetInRadiusWithTag(x, y, 200, "coop_respawn")[1]
     local to_x, to_y = EntityGetTransform(respawn)
     for i, player in ipairs(get_players_including_disabled()) do
         local player_data = Player(player)
